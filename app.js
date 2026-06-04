e825f8e70478ec41d8bfcbfb8aec2f0a8e330666
Rishit-07 <sairishitsunku@gmail.com>
2026-06-04 10:21:05 +0530

    Add dynamic port for deployment

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ app.use(express.json());
 app.use('/users', userRoutes);
 app.use('/posts', postRoutes);
 
-app.listen(3000, () =>
-  console.log('Server running on port 3000')
-);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
